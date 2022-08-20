dc30dd1d41485616f02d97836c07a9c66fca0a4a
balkan <malkan@noon.com>
2022-08-20 13:50:32 +0300

    verbose name

--- listings/models.py
+++ listings/models.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from realtors.models import Realtor
 
 class Listing(models.Model):
-  realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
-  title = models.CharField(max_length=200)
+  realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING, verbose_name="Ekleyen")
+  title = models.CharField(max_length=200, verbose_name="baslik")
   address = models.CharField(max_length=200, verbose_name="adres")
   city = models.CharField(max_length=100, verbose_name="sehir")
   state = models.CharField(max_length=100, verbose_name="sehir")
